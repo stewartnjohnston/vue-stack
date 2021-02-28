3586530155d066d03737226fa0b18be85bf126f6
stewartnjohnston <stewartnjohnston@yahoo.com>
2021-02-28 01:51:00 -0500

    git drive me nuts

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
